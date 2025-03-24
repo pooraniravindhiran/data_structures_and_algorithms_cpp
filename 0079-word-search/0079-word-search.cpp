79ab795254f75918e4ed25ce70aba26593e45090
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2025-03-24 15:33:35 -0700

    Time: 398 ms (51.16%), Space: 11.7 MB (24.68%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,39 +1,37 @@
+// TC- O(m*n*(3^k)) where m,n are board size, k is word length
+// SC- O(k) due to recursion stack
 class Solution {
-private:
-    int rows, cols;
-    vector<int> directions={-1,0,1,0,-1};
-    bool dfs(pair<int,int> cell, vector<vector<char>>& board, string word){
-        if(word.size()==0)
-            return true;
-        
-        // mark visited by some indicator- no need of extra space
-        int i=cell.first;
-        int j=cell.second;
-        char temp = board[i][j];
-        board[i][j] = '#';
-
-        for(int d=0; d<directions.size()-1; d++){
-            int new_i = i+directions[d];
-            int new_j = j+directions[d+1];
-            if(new_i<rows and new_j<cols and new_i>=0 and new_j>=0 and word[0]==board[new_i][new_j]){
-                if(dfs({new_i, new_j}, board, word.substr(1)))
-                    return true;
-            }
-        }
-
-        // after you've checked all paths from curr cell, replace it with original char
-        board[i][j]=temp;
-        return false;
-
-    }
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        rows=board.size();
-        cols=board[0].size();
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
-                if(board[i][j]==word[0]){
-                    if(dfs({i,j}, board, word.substr(1)))
+        int r = board.size();
+        int c = board[0].size();
+
+        vector<int> dir= {-1, 0, 1, 0, -1};
+        function<bool(int, int, int)> dfs= [&](int a, int b, int w){
+            if (word.size() == w)
+                return true;
+
+            char temp = board[a][b];
+            board[a][b] = '#';
+
+            int row, col;
+            for(int k=0; k<dir.size()-1; k++){
+                row= a+dir[k];
+                col= b+dir[k+1];
+                if(row>=0 and col>=0 and row<r and col<c and word[w]==board[row][col]){
+                    if (dfs(row, col, w+1))
+                        return true;
+                }
+            }
+            board[a][b] = temp;
+            return false;
+        };
+
+        // Iterate through board to find starting letter matches and call dfs
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                if (word[0]==board[i][j]){
+                    if (dfs(i, j, 1))
                         return true;
                 }
             }
