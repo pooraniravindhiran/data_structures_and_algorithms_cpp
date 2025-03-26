85413d88fd71f8900fb7dd56e06344c150badde3
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2025-03-26 09:31:37 -0700

    Time: 1988 ms (10.22%), Space: 510.9 MB (7.67%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,18 +1,20 @@
-bool compareSecondElement(vector<int>& a, vector<int>& b){
-        return a[1]<b[1];
-    }
+// TC- O(nlogn)
+// SC - O(logn)
+
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        int result = 0;
-        sort(intervals.begin(), intervals.end(), compareSecondElement);
-        int max = intervals[0][1];
-        for(int i =1; i<intervals.size(); i++){
-            if(intervals[i][0]>=max)
-                max = intervals[i][1];
+        // sort based on end times- we need min number of overlapping intervals - i.e max number of non overalpping intervals- this means given 2 end times, we should also retain one with smaller end time. 
+        sort(intervals.begin(), intervals.end(), [](vector<int> a, vector<int>b){return a[1]<b[1];});
+
+        // decide if current interval is overlapping with most recent end time stored. if not, then update end time. If so, then update ans.
+        int ans = 0; int end = INT_MIN;
+        for(int i=0; i<intervals.size(); i++){
+            if(intervals[i][0] >= end)
+                end = intervals[i][1];
             else
-                result+=1;
+                ans++;
         }
-        return result;
+        return ans;
     }
 };
