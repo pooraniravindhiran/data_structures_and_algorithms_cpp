704ed70206cf9b89306c06f0f5c6e7d64e0761e7
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2025-03-27 23:30:05 -0700

    Time: 72 ms (48.94%), Space: 14.1 MB (78.55%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,31 @@
+// TC- O(n^2) (explanation- 1 + 2+ ... n = n*(n+1)/2 )
+// SC- O(n)
+
+class Solution {
+private:
+    
+public:
+    int lengthOfLIS(vector<int>& nums) {
+
+        // Step 1: Array to represent ans i.e length of longest increasing subsequence
+        // so dp[i] stores the LIS ending at i. ans at dp[n]
+        vector<int> dp(nums.size(), 1);
+
+        // Step 2: Base case
+        // Assign 1 initially as every number could be considered as a subsequence by itself
+
+        // Step 3: Recurrence relation
+        // if nums[i]>nums[j], dp[i] = max(dp[i], dp[j]+1 where j<i)
+        for(int i=1; i<dp.size(); i++){
+            for(int j=0; j<i; j++){
+                if(nums[j]<nums[i])
+                    dp[i] = max(dp[i], dp[j]+1);
+            }
+        }
+
+        int ans = INT_MIN;
+        for(int i=0; i<dp.size(); i++)
+            ans = max(ans, dp[i]);
+        return ans;
+    }
+};
