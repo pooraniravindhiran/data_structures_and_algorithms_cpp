b96dfd7e356ac737b53625a00c7bc374d798989f
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2025-04-09 16:48:42 -0700

    Time: 25 ms (36.09%), Space: 23.4 MB (89.87%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -7,52 +7,44 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
-private:
-    TreeNode* LCA;
-    bool subtreeContainsOneTarget(TreeNode* root, TreeNode* p, TreeNode* q){
-        if (root==NULL)
-            return false;
-        int mid = (root==p or root==q) ? 1:0;
-        int left = (subtreeContainsOneTarget(root->left, p, q)) ? 1:0;
-        int right = (subtreeContainsOneTarget(root->right, p, q)) ? 1:0;
-        if((mid+left+right)==2)
-            LCA = root;
-        return ((mid+left+right)>0);
-    }
-public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        subtreeContainsOneTarget(root, p, q);
-        return LCA;
-    }
-};
 
+// TC- O(n)
+// SC- O(n)
+// Applies to all trees
 // class Solution {
+//     TreeNode* lca;
+//     bool subTreeContainsOneTarget(TreeNode* root, TreeNode* p, TreeNode* q){
+//         if(root==NULL)
+//             return false;
+//         int mid = (root==p or root==q) ? 1:0;
+//         int left = subTreeContainsOneTarget(root->left, p, q) ? 1:0;
+//         int right = subTreeContainsOneTarget(root->right, p, q) ? 1:0;
+//         if(mid+left+right == 2)
+//             lca = root;
+//         return (mid+left+right>0);
+//     }
 // public:
 //     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-//         // Base case: If the root is null, return null
-//         if (root == nullptr) {
-//             return nullptr;
-//         }
-
-//         // Value of the current node (root)
-//         int parentVal = root->val;
-
-//         // Value of p
-//         int pVal = p->val;
-
-//         // Value of q
-//         int qVal = q->val;
-
-//         if (pVal > parentVal && qVal > parentVal) {
-//             // Both p and q are greater than parent, search in the right subtree
-//             return lowestCommonAncestor(root->right, p, q);
-//         } else if (pVal < parentVal && qVal < parentVal) {
-//             // Both p and q are smaller than parent, search in the left subtree
-//             return lowestCommonAncestor(root->left, p, q);
-//         } else {
-//             // We found the split point, the LCA
-//             return root;
-//         }
+//         subTreeContainsOneTarget(root, p, q);
+//         return lca;
 //     }
 // };
+
+// TC- O(n)
+// SC- O(n)
+// Applies to BST
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL)
+            return NULL;
+        int pval = p->val;
+        int qval = q->val;
+        if(pval<root->val and qval<root->val)
+            return lowestCommonAncestor(root->left, p, q);
+        else if(pval>root->val and qval>root->val)
+            return lowestCommonAncestor(root->right, p, q);
+        else 
+            return root;
+    }
+};
