efc75184bc56b9ba64b55616729cc9d7b1518f5b
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2025-04-20 18:44:34 -0700

    Time: 2 ms (59.32%), Space: 17.8 MB (71.85%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,35 +1,30 @@
-// TC- O(nlogk)
 // SC- O(n)
+// TC- O(n)
+
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        if(k==nums.size())
-            return nums;
+        vector<int> res;
 
         // create freq map
-        unordered_map<int, int> freq_map;
-        for(int i=0; i<nums.size(); i++){
-            if(freq_map.find(nums[i])!=freq_map.end())
-                freq_map[nums[i]]++;
-            else
-                freq_map.insert({nums[i], 1});
-        }
+        unordered_map<int, int> freqmap;
+        for(int num:nums)
+            freqmap[num]++;
 
-        // create a heap of size k and push element one by one. Going for min heap as I don't want to store all n elements
-        auto comp = [&freq_map](int n1, int n2){return freq_map[n1]>freq_map[n2];};
-        priority_queue<int , vector<int>, decltype(comp)> min_heap(comp);
-        for(pair<int,int> p:freq_map){
-            min_heap.push(p.first);
+        // create a priority queue based on frequencies
+        auto cmp = [&freqmap](int a, int b){return freqmap[a]>freqmap[b];};
+        priority_queue<int, vector<int>, decltype(cmp)> min_heap(cmp);
+        for(auto pair: freqmap){
+            min_heap.push(pair.first);
             if(min_heap.size()>k)
                 min_heap.pop();
         }
 
-        // return output
-        vector<int> result(k);
-        for(int i=k-1; i>=0; i--){
-            result[i] = min_heap.top();
+        while(!min_heap.empty()){
+            res.push_back(min_heap.top());
             min_heap.pop();
         }
-        return result;
+        reverse(res.begin(), res.end());
+        return res;
     }
 };
