ada944cbbda15aebbf8f039ac4558ed5ad76875a
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2025-08-03 16:07:21 -0700

    Time: 0 ms (100%), Space: 8.7 MB (16.08%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string res;
+        res.reserve(word1.size()+word2.size()); // prevents reallocation
+
+        int idx1=0, idx2=0;
+        while(idx1<word1.size() or idx2<word2.size()){
+            if(idx1<word1.size())
+                res.push_back(word1[idx1++]);
+            if(idx2<word2.size())
+                res.push_back(word2[idx2++]);
+        }
+
+        while(idx1<word1.size())
+            res.push_back(word1[idx1++]);
+        
+        while(idx2<word2.size())
+            res.push_back(word2[idx2++]);
+
+        return res;
+    }
+};
