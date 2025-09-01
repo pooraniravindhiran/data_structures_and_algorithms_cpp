11afde500b85572a0b54386760a46c749fe1ba68
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2025-08-31 21:58:15 -0700

    Time: 101 ms (46.3%), Space: 88.5 MB (8.86%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -9,22 +9,25 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// TC- O(n)
+// SC- O(h)
+
 class Solution {
 private:
     int num_goodNodes = 0;
-    void dfs_helper(TreeNode* node, int max_pathsum){
-        if(node->val==max_pathsum)
+    void dfs_helper(TreeNode* node, int max_pathsum_sofar){
+        if(node->val>=max_pathsum_sofar)
             num_goodNodes++;
         if(node->left)
-            dfs_helper(node->left, max(node->left->val, max_pathsum));
+            dfs_helper(node->left, max(node->val, max_pathsum_sofar));
         if(node->right)
-            dfs_helper(node->right, max(node->right->val, max_pathsum));
+            dfs_helper(node->right, max(node->val, max_pathsum_sofar));
         return;
     }
 public:
     int goodNodes(TreeNode* root) {
         if(root)
-            dfs_helper(root, root->val);
+            dfs_helper(root, INT_MIN);
         return num_goodNodes;
     }
 };
