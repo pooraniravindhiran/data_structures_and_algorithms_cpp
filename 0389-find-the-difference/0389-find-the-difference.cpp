b5cbf01dc1b4d80fcd9db6a1f864cbf66262f79c
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2025-09-02 22:47:35 -0700

    Time: 0 ms (100%), Space: 9.3 MB (29.57%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,15 +1,28 @@
-// TC - O(nlogn)
-// SC - O(logn)
+// // TC - O(nlogn)
+// // SC - O(logn)
+// class Solution {
+// public:
+//     char findTheDifference(string s, string t) {
+//         sort(t.begin(), t.end());
+//         sort(s.begin(), s.end());
+//         for(int i=0; i<s.size(); i++){
+//             if(s[i]!=t[i])
+//                 return t[i];
+//         }
+//         return t.back();
+//     }
+// };
 
+// TC - O(n)
+// SC - O(1)
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(t.begin(), t.end());
-        sort(s.begin(), s.end());
-        for(int i=0; i<s.size(); i++){
-            if(s[i]!=t[i])
-                return t[i];
-        }
-        return t.back();
+        char res = 0; // because 0 when exor'd with anything returns 0
+        for(char c:s)
+            res ^= c;
+        for(char c:t)
+            res ^= c;
+        return res;
     }
 };
