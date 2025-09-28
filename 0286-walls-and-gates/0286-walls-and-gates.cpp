dfcc16476238b7ed8f11b7971c4488c3eb37d670
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2025-09-28 00:07:04 -0700

    Time: 2 ms (84.55%), Space: 19 MB (70.94%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.cpp
+++ 0286-walls-and-gates/0286-walls-and-gates.cpp
@@ -1,6 +1,38 @@
+// TC- O(m*n)
+// SC- O(m*n)
+
 class Solution {
 public:
     void wallsAndGates(vector<vector<int>>& rooms) {
-        
+        // go from gates and add all gates to queue
+        queue<pair<int, int>> q;
+        int r = rooms.size();
+        int c = rooms[0].size();
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                if(rooms[i][j]==0)
+                    q.emplace(i,j);
+            }
+        }
+
+        // do multisource bfs
+        vector<int> dir = {-1, 0, 1, 0, -1};
+        while(!q.empty()){
+            int qsize = q.size();
+            for(int i=0; i<q.size(); i++){
+                int row = q.front().first;
+                int col = q.front().second;
+                q.pop();
+
+                for(int d=0; d<dir.size()-1; d++){
+                    int next_row = row + dir[d];
+                    int next_col = col+ dir[d+1];
+                    if(next_row>=0 and next_row<r and next_col>=0 and next_col<c and rooms[next_row][next_col]>rooms[row][col]+1){
+                        rooms[next_row][next_col] = rooms[row][col]+1;
+                        q.emplace(next_row, next_col);
+                    }
+                }
+            }
+        }
     }
 };
