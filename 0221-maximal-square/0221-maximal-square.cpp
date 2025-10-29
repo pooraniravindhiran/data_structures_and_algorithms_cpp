5509a604ef42bf56a2cb1dce1f0208980db02023
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2025-10-28 17:21:28 -0700

    Time: 31 ms (14.39%), Space: 40.7 MB (5.03%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -0,0 +1,40 @@
+// TC- O(m*n) as time for one row is creating histogram and largestrectangle func = O(n) +O(n)
+// SC- O(n) n+n where n is for stack and histogram
+
+class Solution {
+private:
+    int largestRectangleArea(vector<int> heights) {
+        int maxArea = 0;
+        stack<int> st; // stores indices
+        heights.push_back(0); // to flush out stack at the end
+
+        for(int i=0; i<heights.size(); i++){
+            while(not st.empty() and heights[st.top()]>heights[i]){
+                int height = heights[st.top()];
+                st.pop();
+                int width;
+                int right_boundary = i-1;
+                int left_boundary = st.empty() ? 0:st.top()+1;
+                width = right_boundary-left_boundary+1;
+                int side = min(width, height);
+                maxArea = max(maxArea, side*side);
+            }
+            st.push(i);
+        }
+        return maxArea;
+    }
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int rows=matrix.size();
+        int cols=matrix[0].size();
+        int maxArea = 0;
+        vector<int> heights(cols, 0);
+
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++)
+                heights[j] = matrix[i][j]=='1' ? heights[j]+1:0;
+            maxArea = max(maxArea, largestRectangleArea(heights));
+        }
+        return maxArea;
+    }
+};
