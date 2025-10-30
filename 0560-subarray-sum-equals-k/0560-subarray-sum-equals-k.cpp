82b92ad9bcda804b6cc5aafa0882bd268cf7a561
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2025-10-30 00:51:30 -0700

    Time: 31 ms (96.48%), Space: 45.5 MB (54.5%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,25 +1,55 @@
-// TC- O(n^2)
-// SC- O(1)
+// TC- O(n)
+// SC- O(n)
 
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-        
-        // iterate through every pair and check diff between prefix_sum
+
+        unordered_map<int, int> freqtable;
+        freqtable[0] = 1;
+
+        // if prefix_sum[j]-prefix_sum[i] == 0, then sum of elements lying between these indices is 0. Similarly, sum of elementts lying between two indices is k if prefix_sum[j]-prefix_sum[i] == k.
+        int prefix_sum = 0;
         int ans = 0;
-        for(int start=0; start<n; start++){
-            int sum = 0;
-            for(int end=start; end<n; end++){
-                sum += nums[end];
-                if(sum==k)
-                    ans++;
-            }
+        for(int i=0; i<n; i++){
+            prefix_sum += nums[i];
+
+            if(freqtable.find(prefix_sum-k)!=freqtable.end())
+                ans += freqtable[prefix_sum-k];
+                
+            if(freqtable.find(prefix_sum) != freqtable.end())
+                freqtable[prefix_sum] ++;
+            else
+                freqtable[prefix_sum] = 1;
+                
         }
-        
         return ans;
     }
 };
+
+// // TC- O(n^2)
+// // SC- O(1)
+
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         int n = nums.size();
+        
+//         // iterate through every pair and check diff between prefix_sum
+//         int ans = 0;
+//         for(int start=0; start<n; start++){
+//             int sum = 0;
+//             for(int end=start; end<n; end++){
+//                 sum += nums[end];
+//                 if(sum==k)
+//                     ans++;
+//             }
+//         }
+        
+//         return ans;
+//     }
+// };
 
 
 
