78bb15e8ca571a223cf7d7d925c7a8c22f864bd0
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2025-10-30 00:49:10 -0700

    Time: 2505 ms (8.93%), Space: 34.3 MB (85.8%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,21 +1,18 @@
 // TC- O(n^2)
-// SC- O(n)
+// SC- O(1)
 
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> prefix_sum(n+1, 0);
-
-        // find prefix sum
-        for(int i=1; i<=n; i++)
-            prefix_sum[i] = prefix_sum[i-1]+nums[i-1];
-
+        
         // iterate through every pair and check diff between prefix_sum
         int ans = 0;
         for(int start=0; start<n; start++){
-            for(int end=start+1; end<=n; end++){
-                if(prefix_sum[end]-prefix_sum[start]==k)
+            int sum = 0;
+            for(int end=start; end<n; end++){
+                sum += nums[end];
+                if(sum==k)
                     ans++;
             }
         }
@@ -23,5 +20,33 @@ public:
         return ans;
     }
 };
+
+
+
+// // TC- O(n^2)
+// // SC- O(n)
+
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         vector<int> prefix_sum(n+1, 0);
+
+//         // find prefix sum
+//         for(int i=1; i<=n; i++)
+//             prefix_sum[i] = prefix_sum[i-1]+nums[i-1];
+
+//         // iterate through every pair and check diff between prefix_sum
+//         int ans = 0;
+//         for(int start=0; start<n; start++){
+//             for(int end=start+1; end<=n; end++){
+//                 if(prefix_sum[end]-prefix_sum[start]==k)
+//                     ans++;
+//             }
+//         }
+        
+//         return ans;
+//     }
+// };
 
 
