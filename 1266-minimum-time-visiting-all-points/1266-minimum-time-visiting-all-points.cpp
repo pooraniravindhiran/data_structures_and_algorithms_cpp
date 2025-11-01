1dbd770952f4a9628f0da227bb146ef0339d4238
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2025-11-01 16:22:37 -0700

    Time: 0 ms (100%), Space: 14.6 MB (5.39%) - LeetHub

---
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int mintime = 0;
+        for(int i=1; i<points.size(); i++){
+            vector<int> start = points[i-1];
+            vector<int> end = points[i];
+
+            int dx = abs(end[0]-start[0]);
+            int dy = abs(end[1]-start[1]);
+            // mintime += min(dx, dy) + abs(dx-dy); // max diag moves possible + straight moves
+            mintime += max(dx, dy);
+        }
+        return mintime;
+    }
+};
