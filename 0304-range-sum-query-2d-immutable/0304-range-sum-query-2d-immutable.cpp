a3f383ac2ce87416b86838f6fb2ad98735122711
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2025-11-07 15:07:09 -0800

    Time: 12 ms (94.76%), Space: 152.3 MB (5.91%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -1,26 +1,22 @@
 class NumMatrix {
 private:
-    int m, n;
-    vector<vector<int>> summed;
+    vector<vector<int>> prefix_sum;
 public:
     NumMatrix(vector<vector<int>>& matrix) {
-        m = matrix.size();
-        n= matrix[0].size();
+        int rows = matrix.size()+1;
+        int cols = matrix[0].size()+1;
+        vector<vector<int>> prefix_sum(rows, vector<int> (cols, 0));
 
-        vector<vector<int>> summed(m+1, vector<int>(n+1,0));
-
-        for(int i=0; i<matrix.size(); i++){
-            for(int j=0; j<matrix[0].size(); j++){
-                summed[i+1][j+1] = summed[i+1][j]+summed[i][j+1]-summed[i][j]+matrix[i][j];
+        for(int i=1; i<rows; i++){
+            for(int j=1; j<cols; j++){
+                prefix_sum[i][j] = prefix_sum[i][j-1]+prefix_sum[i-1][j]-prefix_sum[i-1][j-1]+matrix[i-1][j-1];
             }
         }
-        this->summed = summed;
+        this->prefix_sum = prefix_sum;
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        // TC- O(1)
-        // SC- O(1)
-        return summed[row2+1][col2+1]-summed[row1][col2+1]-summed[row2+1][col1]+summed[row1][col1];
+        return prefix_sum[row2+1][col2+1]-prefix_sum[row1][col2+1]-prefix_sum[row2+1][col1]+prefix_sum[row1][col1];
     }
 };
 
