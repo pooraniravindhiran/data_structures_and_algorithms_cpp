d8914a8fffccfb8599afedd049c70317c02b3dd7
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2025-11-08 14:26:05 -0800

    Time: 4 ms (20.16%), Space: 28.3 MB (7.95%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,18 +1,41 @@
 // TC- O(n)
 // SC- O(1)
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int res;
-        int count = 0;
-        for(auto num:nums){
-            if(count==0)
-                res = num;
-            if(res==num)
-                count++;
-            else
-                count--;
+        // at every bit, we check the majority bit
+        int res = 0;
+        for(int i=0; i<32; i++){
+            int bitpos = 1<<i;
+            int bitcount = 0;
+            for(auto num:nums){
+                if(num&bitpos)
+                    bitcount++;
+            }
+            if(bitcount>(nums.size()/2))
+                res |= bitpos;
         }
+        
         return res;
     }
 };
+
+// // TC- O(n)
+// // SC- O(1)
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int res;
+//         int count = 0;
+//         for(auto num:nums){
+//             if(count==0)
+//                 res = num;
+//             if(res==num)
+//                 count++;
+//             else
+//                 count--;
+//         }
+//         return res;
+//     }
+// };
