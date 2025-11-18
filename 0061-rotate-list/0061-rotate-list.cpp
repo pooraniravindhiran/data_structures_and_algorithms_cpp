aba471b7a3227a10c4f191bf1863a89fa465db3c
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2025-11-17 16:17:49 -0800

    Time: 0 ms (100%), Space: 16.3 MB (63.54%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -1,3 +1,5 @@
+// TC- O(n)
+// SC- O(1)
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -11,42 +13,31 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head== NULL or k==0 or head->next==NULL)
-            return head;
         
-        //find len of list
-        ListNode* curr= head;
-        int len=0;
-        while(curr!=NULL)
-        {
+        if(k==0 or head==NULL or head->next==NULL)
+            return head;
+            
+        // Connect last node to head to make a cycle
+        int len = 1; 
+        ListNode* curr = head;
+        while(curr->next){
+            curr= curr->next;
             len++;
+        }
+        curr->next = head;
+
+        // Find new tail
+        k = k%len;
+        int tailpos = len-k-1;
+
+        // Break cycle at new tail
+        curr = head;
+        while(tailpos--)
             curr= curr->next;
-        }
         
-        // find node that will become tail
-        int count=0;
-        curr= head;
-        k= k%len;
-        if(k==0)
-            return head;
-        cout<<k<<endl;
-        while(count<(len-k-1))
-        {
-            curr= curr->next;
-            count++;
-        }
-        
-        // make that as tail and add second half to head
-        ListNode* temp=curr->next;
-        curr->next= NULL;
-        if(temp)
-        {
-            ListNode* new_head= temp;
-            while(temp->next!=NULL)
-                temp=temp->next;
-            temp->next= head;
-            head= new_head;
-        }
-        return head;
+        ListNode* newhead = curr->next;
+        curr->next = NULL;
+        return newhead;
+
     }
 };
