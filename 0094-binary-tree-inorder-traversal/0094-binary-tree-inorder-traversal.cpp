d6f31037f6ca7fb5408270062a82514951bfe9e7
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2025-11-20 13:33:02 -0800

    Time: 0 ms (100%), Space: 10.9 MB (64.44%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,3 +1,6 @@
+// TC- O(n)
+// SC- O(n)
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -9,75 +12,19 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-
- //Morris traversal but modifies tree- O(n), O(1)
 class Solution {
-    vector<int> result;
+private:
+    void helper(TreeNode* node, vector<int>& inorder){
+        if(node==NULL)
+            return;
+        helper(node->left, inorder);
+        inorder.push_back(node->val);
+        helper(node->right, inorder);
+    }
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        TreeNode* curr=root;
-        TreeNode* last;
-        
-        while(curr!=NULL){
-            if(curr->left==NULL){
-                result.push_back(curr->val);
-                curr= curr->right;
-            }
-            else{
-                last= curr->left;
-                while(last->right!=NULL)
-                    last= last->right;
-                last->right= curr;
-                TreeNode* temp= curr;
-                curr= curr->left;
-                temp->left= NULL;
-            }
-
-        }
-        return result;
+        vector<int> inorder;
+        helper(root, inorder);
+        return inorder;
     }
 };
-
-// class Solution {
-//     vector<int> result;
-// public:
-//     vector<int> inorderTraversal(TreeNode* root) {
-//         stack<TreeNode*> s;
-//         TreeNode* curr= root;
-//         while(curr!=NULL or !s.empty()){
-//             if(curr!=NULL){
-//                 s.push(curr);
-//                 curr= curr->left;
-//             }
-//             else{
-//                 TreeNode* top= s.top();
-//                 s.pop();
-//                 result.push_back(top->val);
-//                 curr= top->right;
-//             }
-//         }
-//         return result;
-//     }
-// };
-// class Solution {
-//     vector<int> result;
-// public:
-//     vector<int> inorderTraversal(TreeNode* root) {
-//         if(root==NULL)
-//             return result;
-//         stack<TreeNode*> s;
-//         TreeNode* curr= root;
-//         while(curr!=NULL or !s.empty()){
-//             while(curr!=NULL){
-//                 s.push(curr);
-//                 curr= curr->left;
-//             }
-//             curr= s.top();
-//             s.pop();
-//             result.push_back(curr->val);
-//             curr= curr->right;
-//         }
-//         return result;
-//     }
-// };
