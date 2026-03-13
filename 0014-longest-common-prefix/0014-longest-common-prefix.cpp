d71cdc41cac008538ad9978a20cd3307dc907c80
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-03-12 19:19:05 -0700

    Time: 0 ms (100%), Space: 11.9 MB (45.03%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,5 +1,9 @@
+// Horizontal scanning
 // TC- O(n*m) where n is number of words and m is max num of characters in individual string
 // SC- O(1)
+
+// There's also vertical scanning technique, same TC and SC.
+// There's also sorting (sort all strings, and just compare first and last str) which gives better TC - O(nlogn+m)
 
 class Solution {
 public:
@@ -9,13 +13,9 @@ public:
         string lcp = strs[0];
 
         for(int i=1; i<strs.size(); i++){
-            int j;
-            for(j=0; j<min(lcp.size(), strs[i].size()); ){
-                if(lcp[j]==strs[i][j])
-                    j++;
-                else
-                    break;
-            }
+            int j=0;
+            while(j<min(lcp.size(), strs[i].size()) && lcp[j]==strs[i][j])
+                j++;
             lcp = lcp.substr(0, j);
         }
         return lcp;
