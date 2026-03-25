1c6ee6e425f22a0024d5414f7c883ff6ea5ce4cb
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-03-25 13:21:50 -0700

    Time: 3 ms (31.55%), Space: 22.3 MB (50.54%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -5,19 +5,41 @@
 #         self.left = left
 #         self.right = right
 
-# TC- O(n)
-# SC- O(n)
+# # TC- O(n)
+# # SC- O(n)
+
+# class Solution:
+#     def inorderTraversal(self, node:Optional[TreeNode], nodes)->None:
+#         if node is None:
+#             return
+#         self.inorderTraversal(node.left, nodes)
+#         nodes.append(node.val)
+#         self.inorderTraversal(node.right, nodes)
+
+#     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+#         nodes = []
+#         self.inorderTraversal(root, nodes)
+#         return nodes[k-1]
+
+# TC- O(h+k)
+# SC- O(h)
 
 class Solution:
-    def inorderTraversal(self, node:Optional[TreeNode], nodes)->None:
-        if node is None:
-            return
-        self.inorderTraversal(node.left, nodes)
-        nodes.append(node.val)
-        self.inorderTraversal(node.right, nodes)
-
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        nodes = []
-        self.inorderTraversal(root, nodes)
-        return nodes[k-1]
+        self.count = 0
+        self.ans = None
+
+        def inorderTraversal(node):
+            if node is None:
+                return
+            inorderTraversal(node.left)
+            self.count += 1
+            if self.count==k:
+                self.ans= node.val
+                return
+            inorderTraversal(node.right)
+
+        inorderTraversal(root)
+        return self.ans
+        
         
