354966bf4f11644e66d5703cf4c70dcc9d6d9cb3
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-04-05 13:53:25 -0700

    Time: 9 ms (98.4%), Space: 19.4 MB (67.53%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,27 @@
+# TC- O(m+n)
+# SC- O(1)
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        left, right = 0, len(s1)-1
+
+        # count letters in s1
+        count = [0]*26
+        for ch in s1:
+            count[ord(ch)-ord('a')]+= 1
+        
+        # slide window over s2 and compare each window
+        window = [0]*26
+        for ch in s2[0:len(s1)]:
+            window[ord(ch)-ord('a')]+=1
+        
+        if count==window:
+            return True
+        
+        for i in range(len(s1), len(s2)):
+            window[ord(s2[i])-ord('a')] += 1 # add new char
+            window[ord(s2[i-len(s1)])-ord('a')] -= 1 # remove old char
+            if count==window:
+                return True
+        return False
+        
