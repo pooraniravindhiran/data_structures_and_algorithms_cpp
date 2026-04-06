535c4b7ecf973e0ae2c7ac08289c93a8d055aa75
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-04-06 09:44:08 -0700

    Time: 0 ms (100%), Space: 19.2 MB (67.69%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -1,7 +1,12 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        while(b):
+        mask = 0xffffffff
+        # in python, there's no fixed 32 bit precision. so -1 is infinite precision. so b would never become 0. so i simulate 32 bit by masking
+        while b:
             temp = a ^ b
             b = (a & b) <<1
-            a = temp
-        return a
+            b = b & mask
+            a = temp & mask
+
+        # but masking makes the negative number reallly large, so handle that
+        return a if a<=0x7fffffff else ~(a^mask)
