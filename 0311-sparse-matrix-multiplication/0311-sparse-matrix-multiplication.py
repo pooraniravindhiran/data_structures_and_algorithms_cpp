4ac25d5bccfc4861c13be560fb32137860a3f145
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-04-06 11:31:02 -0700

    Time: 0 ms (100%), Space: 19.7 MB (40.65%) - LeetHub

---
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
@@ -0,0 +1,58 @@
+class Solution:
+    def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
+        # Represent the 1st matrix in CSR format and 2nd matrix in CSC format
+        def to_csr(mat):
+            rows, cols, data = [], [], []
+            rows.append(0)
+            for r in range(len(mat)):
+                for c in range(len(mat[0])):
+                    if mat[r][c]!=0:
+                        cols.append(c)
+                        data.append(mat[r][c])
+                rows.append(len(data)) 
+            return rows, cols, data
+        
+        def to_csc(mat):
+            rows, cols, data = [], [], []
+            cols.append(0)
+            for c in range(len(mat[0])):
+                for r in range(len(mat)):
+                    if mat[r][c]!=0:
+                        rows.append(r)
+                        data.append(mat[r][c])
+                cols.append(len(data)) 
+            return rows, cols, data
+
+        rows1, cols1, data1 = to_csr(mat1)
+        rows2, cols2, data2 = to_csc(mat2)
+
+        # print(rows1, cols2, data2)
+
+        # multiply given representations
+        r1, c1 = len(mat1), len(mat1[0])
+        r2, c2 = len(mat2), len(mat2[0])
+        ans = [[0]*c2 for _ in range(r1)]
+        
+        for i in range(r1):
+            for j in range(c2):
+                p1 = rows1[i]
+                p1_end = rows1[i+1]
+                p2 = cols2[j]
+                p2_end = cols2[j+1]
+
+                dot_pdt = 0
+                while p1<p1_end and p2<p2_end:
+                    if cols1[p1] == rows2[p2]:
+                        dot_pdt += data1[p1]*data2[p2]
+                        p1 += 1
+                        p2 += 1
+                    elif cols1[p1]<rows2[p2]:
+                        p1 += 1
+                    else:
+                        p2 += 1
+                ans[i][j] = dot_pdt
+        
+        return ans
+
+
+        
