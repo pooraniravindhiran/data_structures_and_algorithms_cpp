8152a7cfe98f2162258993353bc1431d37f72587
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-04-09 17:15:13 -0700

    Time: 246 ms (53.53%), Space: 21.5 MB (93.7%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,24 +2,23 @@
 # SC- O(mn)
 
 class Solution:
-    def dfs(self, grid, r, c, rows, cols):
-        grid[r][c] = "0"
-        dirs = [-1, 0, 1, 0, -1]
-        for d in range(len(dirs)-1):
-            next_r = r + dirs[d]
-            next_c = c + dirs[d+1]
-
-            if(next_r>=0 and next_r<rows and next_c>=0 and next_c<cols and grid[next_r][next_c]=="1"):
-                self.dfs(grid, next_r, next_c, rows, cols)
-
     def numIslands(self, grid: List[List[str]]) -> int:
         ans = 0
         rows, cols = len(grid), len(grid[0])
 
+        def dfs(r, c):
+            grid[r][c] = '0'
+            dirs = [-1, 0, 1, 0, -1]
+            for d in range(len(dirs)-1):
+                next_r = r+dirs[d]
+                next_c = c+dirs[d+1]
+                if(next_r>=0 and next_c>=0 and next_r<rows and next_c<cols and grid[next_r][next_c]=='1'):
+                    dfs(next_r, next_c)
+
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c]=="1":
+                if grid[r][c]=='1':
                     ans += 1
-                    self.dfs(grid, r, c, rows, cols)
-
+                    dfs(r, c)
+        
         return ans
