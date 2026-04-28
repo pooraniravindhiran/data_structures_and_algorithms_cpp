a04629df81d02df0c3a53339127884296c2c2142
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-04-28 11:50:15 -0700

    Time: 0 ms (100%), Space: 19.3 MB (41.43%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        for idx, curr in enumerate(nums):
+            prev_num = nums[idx-1] if idx>=1 else float('-inf')
+            next_num = nums[idx+1] if idx<n-1 else float('-inf')
+            # print(prev_num, next_num)
+            if curr>prev_num and curr>next_num:
+                return idx
