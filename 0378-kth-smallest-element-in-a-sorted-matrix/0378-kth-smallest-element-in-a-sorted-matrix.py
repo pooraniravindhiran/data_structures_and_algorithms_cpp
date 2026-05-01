4a86109e8ba0f95bad325b1014e9b33458b7e5ad
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-04-30 22:44:27 -0700

    Time: 0 ms (100%), Space: 22.8 MB (90.85%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,27 +1,30 @@
-# SC- O(1)
-# TC- O(n * log(max-min))
+# if k is small, we can go with priority queue approach
+# generally better approach is binary search
 
+# TC- O(nlog(max-min))
+# SC- O(1)
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         n = len(matrix)
 
-        def count_less_equal(mid):
+        def count_less_than_equal(mid):
+            # take top-right or left bottom as ref for efficient counting
             count = 0
-            r, c = n-1, 0
-            while(r>=0 and c<n):
+            r, c = 0, n-1
+            while c>=0 and r<n:
                 if(matrix[r][c]<=mid):
-                    count += r+1
-                    c +=1
+                    count += c+1
+                    r += 1
                 else:
-                    r -= 1
+                    c -= 1
             return count
 
-        low, high = matrix[0][0], matrix[-1][-1]
-        while(low<high):
-            mid = (low+high)//2
-            if count_less_equal(mid)<k:
-                low = mid+1
+        # do binary search on values and not indices because the matrix does not give a sorted array when flattened
+        left, right = matrix[0][0], matrix[-1][-1]
+        while(left<right):
+            mid = left + (right-left)//2
+            if count_less_than_equal(mid)<k:
+                left = mid+1
             else:
-                high = mid
-
-        return low
+                right = mid
+        return left
