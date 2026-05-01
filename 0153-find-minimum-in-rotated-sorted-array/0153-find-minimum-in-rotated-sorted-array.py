93b2232812c279d7a21700a143f20c19ff8d4e6c
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-04-30 23:58:13 -0700

    Time: 0 ms (100%), Space: 19.4 MB (66.68%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -3,27 +3,28 @@
 
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        
         left = 0
         right = len(nums)-1
-
-        # if not rotated
-        if nums[left]<nums[right]:
-            return nums[0]
         
-        # if rotated
         while(left<right):
-            mid = left+ (right-left)//2
-            if nums[mid]>nums[mid+1]: # 3 4 5 1 2
-                return nums[mid+1]
-            
-            elif nums[mid]<nums[mid-1]: # 4 5 1 2 3
-                return nums[mid]
-
-            elif nums[left]>nums[mid]: # 5 1 2 3 4
+            mid = left + (right-left)//2
+            if nums[mid]>nums[right]:
+                left = mid +1
+            else:
                 right = mid
-            
-            else:   # 2 3 4 5 1
-                left = mid+1
+        return nums[left]
+
+# class Solution:
+#     def findMin(self, nums: List[int]) -> int:
+#         left = 0
+#         right = len(nums)-1
+        
+#         while(left<right):
+#             if nums[left]<nums[right]:
+#                 return nums[left]
+#             mid = left + (right-left)//2
+#             if nums[mid]>=nums[left]:
+#                 left = mid +1
+#             else:
+#                 right = mid
+#         return nums[left]
