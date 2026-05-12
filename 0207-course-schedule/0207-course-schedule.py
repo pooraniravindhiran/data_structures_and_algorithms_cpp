b86430cedb7cecd1a5c7bc5b92d82a48bc19c1a1
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-05-12 13:01:32 -0700

    Time: 7 ms (45.41%), Space: 21.4 MB (19.88%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,30 +1,33 @@
-# TC- O(v+e)
-# SC- O(v+e)
+# TC- O(V+E)
+# SC- O(E+V)
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # the problem can be formulated into a directed graph and we need to ultimately find if there's a cycle or not
-        adj_mat = defaultdict(list)
-        for prereq in prerequisites:
-            adj_mat[prereq[0]].append(prereq[1])
-        
-        state = [0]*numCourses # 0 = unvisited, 1 = visiting, 2 = done
+        # create an adj matrix and for each course, do a traversal. If, at any point, you find a loop, then return False. Else, return True. the problem can be formulated into a directed graph and we need to ultimately find if there's a cycle or not
 
-        def has_cycle(n):
-            if state[n]==2:
+        adj_mat = defaultdict(list)
+        for course, prereq in prerequisites:
+            adj_mat[prereq].append(course)
+        
+        # 0 = unvisited, 1 = visiting, 2 = done
+        state = [0]*numCourses
+
+        def has_cycle(i):
+            if state[i]==2:
                 return False
-            elif state[n]==1:
+            
+            if state[i]==1:
                 return True
             
-            state[n]=1
-            for neigh in adj_mat[n]:
-                if has_cycle(neigh):
+            state[i] = 1
+            for course in adj_mat[i]:
+                if has_cycle(course):
                     return True
-            state[n]=2
+
+            state[i] = 2
             return False
 
-        for n in range(numCourses):
-            if state[n]==0:
-                if has_cycle(n):
+        for i in range(numCourses):
+            if state[i]==0 and has_cycle(i):
                     return False
         return True
