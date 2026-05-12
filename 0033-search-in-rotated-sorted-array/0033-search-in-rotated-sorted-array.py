4e64196f70834ac63a069eea7f71be35774ff7c6
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-05-12 13:16:41 -0700

    Time: 0 ms (100%), Space: 19.6 MB (10.15%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -3,6 +3,7 @@
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        # we can do binary search for efficiency. But to figure out which half we need to search, we need to first identify which half is sorted. Once you identify teh sorted half, you compare with target value in that half.
         left, right = 0, len(nums)-1
 
         while left<=right:
@@ -11,11 +12,14 @@ class Solution:
             if nums[mid]==target:
                 return mid
             
+            # left half is sorted
             if nums[left]<=nums[mid]:
                 if nums[left]<=target<nums[mid]:
                     right = mid-1
                 else:
                     left = mid+1
+            
+            # right half (including mid) is not sorted
             else:
                 if nums[mid]<target<=nums[right]:
                     left = mid+1
