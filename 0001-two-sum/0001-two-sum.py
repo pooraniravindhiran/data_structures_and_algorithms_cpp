926b178a846d13096875b39948d9d7689b7e1323
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-05-12 14:10:10 -0700

    Time: 0 ms (100%), Space: 20.4 MB (42.88%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,11 @@
+# TC- O(n)
+# SC- O(n)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        visited = {}
+        for idx, num in enumerate(nums):
+            if target-num in visited:
+                return [idx, visited[target-num]]
+            
+            visited[num] = idx
