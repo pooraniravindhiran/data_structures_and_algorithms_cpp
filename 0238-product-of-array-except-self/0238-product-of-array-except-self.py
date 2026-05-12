e9487fbe48bc21d8bb468c02ba2a53211ed93571
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-05-12 13:56:56 -0700

    Time: 27 ms (44.23%), Space: 25.7 MB (44.08%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,32 +3,17 @@
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        res = [1] * n 
-        for i in range(1, n):
-            res[i] = res[i-1]*nums[i-1]
+
+        ans = [1]*len(nums)
+
+        # find prefix product
+        for i in range(1, len(nums)):
+            ans[i] = ans[i-1]*nums[i-1]
         
+        # find suffix product
         suffix = 1
-        for i in range(n-1, -1, -1):
-            res[i] *= suffix
+        for i in range(len(nums)-1, -1, -1):
+            ans[i] *= suffix
             suffix *= nums[i]
-
-        return res
-
-# TC- O(n)
-# SC- O(n)
-# class Solution:
-#     def productExceptSelf(self, nums: List[int]) -> List[int]:
-#         n = len(nums)
-#         prefix = [1]*n 
-#         for i in range(1, n):
-#             prefix[i] = prefix[i-1]*nums[i-1]
         
-#         suffix = [1] * n
-#         for i in range(n-2, -1, -1):
-#             suffix[i] = suffix[i+1]*nums[i+1]
-
-#         res = [1] * n
-#         for i in range(n):
-#             res[i] = prefix[i]*suffix[i]
-#         return res
+        return ans
