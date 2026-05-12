0dd887713a4d96258528109ee893aea73a4d948c
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-05-12 13:25:49 -0700

    Time: 0 ms (100%), Space: 22.8 MB (62.54%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,14 +3,16 @@
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = Counter(nums)
-        buckets = [[] for i in range(len(nums)+1)]
-        for num, freq in count.items():
-            buckets[freq].append(num)
+        # Count the freq of each element, then put it in a minheap sorted by freq, limit the minheap size by k and return the heap.
 
-        res = []
-        for freq in range(len(buckets)-1, 0, -1):
-            for num in buckets[freq]:
-                res.append(num)
-                if len(res)==k:
-                    return res
+        freq_map = Counter(nums)
+
+        min_heap = []
+        for num, freq in freq_map.items():
+            heapq.heappush(min_heap, (freq, num))
+            if len(min_heap)>k:
+                heapq.heappop(min_heap)
+        
+        ans = [num for (freq, num) in min_heap]
+        return ans
+
