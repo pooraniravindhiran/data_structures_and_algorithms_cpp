279100dfc258d28763fbc2bdf95b7216db17a674
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-05-13 16:22:46 -0700

    Time: 47 ms (34.5%), Space: 22.3 MB (10.09%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -1,32 +1,71 @@
 # SC- O(n)
+class Node:
+    def __init__(self, url):
+        self.url = url
+        self.prev = None
+        self.next = None
 
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        self.history = [homepage]
-        self.curr = 0
+        self.curr = Node(homepage)
 
     def visit(self, url: str) -> None:
-        # TC- O(n)
-        # remove forward history
-        self.history = self.history[:self.curr+1]
-
-        self.history.append(url)
-        self.curr += 1
+        # TC- O(1)
+        node = Node(url)
+        self.curr.next = node
+        node.prev = self.curr
+        self.curr = node
 
     def back(self, steps: int) -> str:
         # TC- O(1)
-        self.curr = max(0, self.curr-steps)
-        return self.history[self.curr]
+        while(steps>0 and self.curr.prev):
+            self.curr = self.curr.prev
+            steps -= 1
+        return self.curr.url
 
     def forward(self, steps: int) -> str:
         # TC- O(1)
-        self.curr = min(len(self.history)-1, self.curr+steps)
-        return self.history[self.curr]
-
+        while(steps>0 and self.curr.next):
+            self.curr = self.curr.next
+            steps -= 1
+        return self.curr.url
 
 # Your BrowserHistory object will be instantiated and called as such:
 # obj = BrowserHistory(homepage)
 # obj.visit(url)
 # param_2 = obj.back(steps)
 # param_3 = obj.forward(steps)
+
+# # SC- O(n)
+
+# class BrowserHistory:
+
+#     def __init__(self, homepage: str):
+#         self.history = [homepage]
+#         self.curr = 0
+
+#     def visit(self, url: str) -> None:
+#         # TC- O(n)
+#         # remove forward history
+#         self.history = self.history[:self.curr+1]
+
+#         self.history.append(url)
+#         self.curr += 1
+
+#     def back(self, steps: int) -> str:
+#         # TC- O(1)
+#         self.curr = max(0, self.curr-steps)
+#         return self.history[self.curr]
+
+#     def forward(self, steps: int) -> str:
+#         # TC- O(1)
+#         self.curr = min(len(self.history)-1, self.curr+steps)
+#         return self.history[self.curr]
+
+
+# # Your BrowserHistory object will be instantiated and called as such:
+# # obj = BrowserHistory(homepage)
+# # obj.visit(url)
+# # param_2 = obj.back(steps)
+# # param_3 = obj.forward(steps)
