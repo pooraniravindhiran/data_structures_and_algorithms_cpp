a47d9deb0d533e83d8c790f546403f5a2d71597f
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-05-13 16:57:24 -0700

    Time: 0 ms (100%), Space: 19.3 MB (77.63%) - LeetHub

--- 0362-design-hit-counter/0362-design-hit-counter.py
+++ 0362-design-hit-counter/0362-design-hit-counter.py
@@ -1,22 +1,46 @@
-# many ts, each ts might have many hits
-# dict but more than 300, delete is difficult
-# queue is better
-
 # SC- O(n)
 class HitCounter:
 
     def __init__(self):
-        self.q = deque()
+        self.times = [0]*300
+        self.hits = [0]*300
 
     def hit(self, timestamp: int) -> None:
         # TC- O(1)
-        self.q.append(timestamp)
+        idx = timestamp%300
+        if self.times[idx]!=timestamp:
+            self.times[idx]= timestamp
+            self.hits[idx] = 1
+        else:
+            self.hits[idx] += 1
 
     def getHits(self, timestamp: int) -> int:
         # TC- O(1)
-        while self.q and self.q[0]<=timestamp-300:
-            self.q.popleft()
-        return len(self.q)
+        total = 0
+        for i in range(300):
+            if timestamp-self.times[i]<300:
+                total += self.hits[i]
+        return total
+
+# # many ts, each ts might have many hits
+# # dict but more than 300, delete is difficult
+# # queue is better
+
+# # SC- O(n)
+# class HitCounter:
+
+#     def __init__(self):
+#         self.q = deque()
+
+#     def hit(self, timestamp: int) -> None:
+#         # TC- O(1)
+#         self.q.append(timestamp)
+
+#     def getHits(self, timestamp: int) -> int:
+#         # TC- O(1)
+#         while self.q and self.q[0]<=timestamp-300:
+#             self.q.popleft()
+#         return len(self.q)
         
 
 # Your HitCounter object will be instantiated and called as such:
