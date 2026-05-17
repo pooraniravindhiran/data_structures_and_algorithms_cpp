61a04cbe0a103e71e27b27ef2a6ba44ac550e132
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-05-17 14:34:43 -0700

    Time: 135 ms (39.31%), Space: 78.9 MB (7.37%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,21 +1,21 @@
-# SC- O(n)
 class Node:
     def __init__(self, key, val):
-        self.key = key
         self.val = val
+        self.key = key
         self.prev = None
         self.next = None
 
+# SC- O(capacity)
 class LRUCache:
     def __init__(self, capacity: int):
-        # dict to store key and value
-        # list due to O(1)
+        # O(1) for lookup of key
+        # O(1) for removal of first key
+        # O(1) for moving arbitrary key to beginning
+        # for these, use dict and dll
         self.capacity = capacity
-        self.dict = {} #key, node
-
-        # dummy head and tail
-        self.head = Node(0,0)
-        self.tail = Node(0,0)
+        self.dict = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
         self.tail.prev = self.head
         self.head.next = self.tail
 
@@ -28,87 +28,36 @@ class LRUCache:
         node.prev = self.head
         self.head.next.prev = node
         self.head.next = node
-        
+
     def get(self, key: int) -> int:
         # TC- O(1)
-        # return val or -1
-        if key not in self.dict:
-            return -1
-        
-        # make it recent
-        node = self.dict[key]
-        
-        self._remove(node)
-        self._add(node)
-
-        return node.val
+        if key in self.dict:
+            node = self.dict[key]
+            # move node to front
+            self._remove(node)
+            self._add(node)
+            return node.val
+        return -1
 
     def put(self, key: int, value: int) -> None:
         # TC- O(1)
         if key in self.dict:
-            # update key in dict
             node = self.dict[key]
+            node.val = value
             self._remove(node)
-            self.dict.pop(key)
+            self._add(node)
+        else:
+            node = Node(key, value)
+            self.dict[key] = node
+            self._add(node)
 
-        # prune dict and order
-        if len(self.dict)>=self.capacity:
-            key_to_remove = self.tail.prev.key
-            self._remove(self.tail.prev)
-            self.dict.pop(key_to_remove)
-
-        node = Node(key, value)
-        self.dict[key] = node
-        self._add(node)
+        if len(self.dict)>self.capacity:
+            if self.tail.prev!=self.head:
+                lru = self.tail.prev
+                self._remove(self.tail.prev)
+                del self.dict[lru.key]
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
-
-# # SC- O(n)
-# class LRUCache:
-
-#     def __init__(self, capacity: int):
-#         # dict to store key and value
-#         # deque instead of heap due to O(1)
-#         self.capacity = capacity
-#         self.dict = {}
-#         self.order = deque()
-
-#     def get(self, key: int) -> int:
-#         # TC- O(n)
-#         # return val or -1
-#         if key not in self.dict:
-#             return -1
-        
-#         # make it recent
-#         self.order.remove(key)
-#         self.order.append(key)
-
-#         return self.dict[key]
-
-#     def put(self, key: int, value: int) -> None:
-#         # TC- O(n)
-#         if key in self.dict:
-#             # update key in dict
-#             self.dict[key] = value
-
-#             # update order
-#             self.order.remove(key)
-#             self.order.append(key)
-
-#         else:
-
-#             # prune dict and order
-#             if len(self.order)>=self.capacity:
-#                 key_to_remove = self.order.popleft()
-#                 self.dict.pop(key_to_remove)
-
-#             self.dict[key] = value
-#             self.order.append(key)
-
-# # Your LRUCache object will be instantiated and called as such:
-# # obj = LRUCache(capacity)
-# # param_1 = obj.get(key)
-# # obj.put(key,value)
