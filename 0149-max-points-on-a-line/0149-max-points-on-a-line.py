6b8a67ade417580b1aacd931f8cbdd807d4224be
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-05-17 12:55:48 -0700

    Time: 28 ms (53.65%), Space: 19.4 MB (66.55%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -8,7 +8,7 @@ class Solution:
         n = len(points)
 
         for i in range(n):
-            slope_count = defaultdict(int)
+            slope_count = defaultdict(int) # create dict inside this loop because different lines can have same slope
             curr_max = 0
 
             for j in range(i+1, n):
@@ -16,17 +16,20 @@ class Solution:
                 pt_1, pt_2 = points[i], points[j]
                 dy = pt_1[1]-pt_2[1]
                 dx = pt_1[0]-pt_2[0]
-                gcd = math.gcd(dy, dx)
+                gcd = math.gcd(dy, dx) # (1,2), (2,4) are pts with same slope, so divide by gcd
 
                 # handle vertical line
                 if dx==0:
                     slope = (1, 0)
+                
+                # handle order of slope
                 elif dx<0:
                     dx = -1*dx
                     dy = -1*dy
                     slope = (dy//gcd, dx//gcd)
+                    
                 else:
-                    slope = (dy/gcd, dx/gcd ) #store ratio instead of float to avoid precision issue
+                    slope = (dy//gcd, dx//gcd ) #store ratio instead of float to avoid precision issue
                 
                 slope_count[slope] += 1
                 curr_max = max(curr_max, slope_count[slope])
