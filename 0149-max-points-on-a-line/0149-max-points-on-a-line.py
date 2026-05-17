ef292cbc75ba2f3a3977cbe6ace3061ab0981a42
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-05-17 12:50:00 -0700

    Time: 28 ms (53.65%), Space: 19.4 MB (66.55%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -0,0 +1,37 @@
+# SC- O(n)
+# TC- O(n^2)
+
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        # compare every pt with every other pt, compute slope and update count
+        maxpts = 0
+        n = len(points)
+
+        for i in range(n):
+            slope_count = defaultdict(int)
+            curr_max = 0
+
+            for j in range(i+1, n):
+
+                pt_1, pt_2 = points[i], points[j]
+                dy = pt_1[1]-pt_2[1]
+                dx = pt_1[0]-pt_2[0]
+                gcd = math.gcd(dy, dx)
+
+                # handle vertical line
+                if dx==0:
+                    slope = (1, 0)
+                elif dx<0:
+                    dx = -1*dx
+                    dy = -1*dy
+                    slope = (dy//gcd, dx//gcd)
+                else:
+                    slope = (dy/gcd, dx/gcd ) #store ratio instead of float to avoid precision issue
+                
+                slope_count[slope] += 1
+                curr_max = max(curr_max, slope_count[slope])
+        
+            maxpts = max(maxpts, curr_max+1)
+        return maxpts
+
+
