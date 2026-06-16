f286a6a65c4bb9af7aa9a62c24ade8fd4735fb40
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-06-16 13:24:46 -0700

    Time: 96 ms (29.37%), Space: 31.5 MB (96.61%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.py
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.py
@@ -1,26 +1,17 @@
-# TC- O(n)
-# SC- O(n)
-
 class Solution:
     def canSeePersonsCount(self, heights: List[int]) -> List[int]:
-        n = len(heights)
-        potentially_visible = [] # monotonic decreasing stack representing potential people that can be seen. Top of stack is the immediate right person whom you can always see. Deeper in the stack are people whom you can potentially see.
-        ans = [0 for _ in range(n)]
+        
+        ans = [0]*len(heights)
+        stack = []
 
-        for i in range(n-1, -1, -1):
-            count = 0
-
-            # calculate count
-            while potentially_visible and heights[i] > potentially_visible[-1]:
-                count += 1
-                potentially_visible.pop()
-            if potentially_visible:
-                count += 1
-
-            # update count
-            ans[i] = count
-
-            # update stack
-            potentially_visible.append(heights[i])
-
+        for i in range(len(heights)-1, -1, -1):
+            curr_ht = heights[i]
+            while stack and curr_ht >= heights[stack[-1]]:
+                stack.pop()
+                ans[i] += 1
+            if stack:
+                ans[i]+=1
+            stack.append(i)
         return ans
+
+        
