3596c42097d979a5cd8559a30a6c51e06e2e4666
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-06-16 14:06:40 -0700

    Time: 0 ms (100%), Space: 19.4 MB (28.27%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,18 +1,32 @@
-# TC- O(mn)
+# TC- O(n)
 # SC- O(n)
 
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums2_map = {val:idx for idx, val in enumerate(nums2)}
+        nums2_map = {}
+        stack = []
+        for num in nums2:
+            while stack and num>stack[-1]:
+                nums2_map[stack.pop()] = num
+            stack.append(num)
+        # print(nums2_map)
+        
+        return [nums2_map.get(num, -1) for num in nums1]
 
-        ans = [-1]*len(nums1)
-        for i in range(len(nums1)):
+# # TC- O(mn)
+# # SC- O(n)
 
-            idx_nums2 = nums2_map[nums1[i]]+1
-            while(idx_nums2<len(nums2) and nums2[idx_nums2]<=nums1[i]):
-                idx_nums2 += 1
+# class Solution:
+#     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+#         nums2_map = {val:idx for idx, val in enumerate(nums2)}
+
+#         ans = [-1]*len(nums1)
+#         for i in range(len(nums1)):
+
+#             idx_nums2 = nums2_map[nums1[i]]+1
+#             while(idx_nums2<len(nums2) and nums2[idx_nums2]<=nums1[i]):
+#                 idx_nums2 += 1
             
-            if idx_nums2<len(nums2):
-                ans[i] = nums2[idx_nums2]
-        return ans
-                
+#             if idx_nums2<len(nums2):
+#                 ans[i] = nums2[idx_nums2]
+#         return ans
