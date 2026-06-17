aff2d4c82b416cb8c67a99617fc8d5ac62165da7
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-06-16 18:30:00 -0700

    Time: 153 ms (27.85%), Space: 68.3 MB (91.69%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,31 +1,30 @@
-# SC- O(n)
-
 class TimeMap:
+
     def __init__(self):
-        self.key_map = defaultdict(list)
-
-    # TC- O(1)
-    def set(self, key: str, value: str, timestamp: int) -> None:
-        self.key_map[key].append((timestamp, value))
-
-    # TC- O(logn)
-    def get(self, key: str, timestamp: int) -> str:
-        if key not in self.key_map:
-            return ""
         
-        res = ""
-        arr = self.key_map[key]
+        # key - multiple values based on ts
+        self.map = defaultdict(list)
 
-        # do binary search on value list
-        left, right = 0, len(arr)-1
-        while left<=right:
-            mid = left+(right-left)//2
-            if arr[mid][0]>timestamp:
-                right = mid-1
-            else:
-                res = arr[mid][1]
-                left = mid+1
-        return res
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.map[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.map or len(self.map[key])==0:
+            return ""
+        lst = self.map[key]
+        if lst:
+            left, right = 0, len(lst)-1
+            ans = ""
+            while left<=right:
+                mid = left+(right-left)//2
+                if lst[mid][0]>timestamp:
+                    right = mid-1
+                else:
+                    ans = lst[mid][1]
+                    left = mid+1
+            return ans
+        else:
+            return ""
 
 
 # Your TimeMap object will be instantiated and called as such:
