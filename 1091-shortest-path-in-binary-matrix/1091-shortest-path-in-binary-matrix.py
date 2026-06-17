f845a253520ffc075a99b63885a9eb45377a9c27
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-06-17 14:39:43 -0700

    Time: 135 ms (71.22%), Space: 19.8 MB (75.83%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,35 +1,34 @@
-# TC- O(n^2)
-# SC- O(n^2)
+# TC- O(mn)
+# SC- O(mn)
 
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        # do bfs, track visited by making it 1
+        
         n = len(grid)
-        if grid[0][0]!=0 or grid[n-1][n-1]!=0:
-            return -1
-        if len(grid)==1:
-            return 1
-        
-        src, dst = (0,0), (n-1, n-1)
-        q = deque()
-        q.append(src)
-        grid[0][0] = 1
-        dirs = [(-1,0), (1,0), (0,1), (0,-1), (-1,1), (-1,-1), (1,-1), (1,1)]
-        dist = 1
-        while q:
-            q_size = len(q)
-            dist += 1
-            for _ in range(q_size):
-                curr_r, curr_c = q[0]
-                q.popleft()
-                for d in dirs:
-                    next_r = curr_r+d[0]
-                    next_c = curr_c+d[1]
-                    if (next_r, next_c) == dst:
-                        return dist
-                    if 0<=next_r<n and 0<=next_c<n and grid[next_r][next_c]==0:
-                        q.append((next_r, next_c))
-                        grid[next_r][next_c] = 1
-        return -1
-        
+        dirs = [(-1, 0), (1, 0), (0, 1), (0, -1), (-1, 1), (1, -1), (-1, -1), (1, 1)]
+        src, dst = (0, 0), (n-1, n-1)
 
+        if grid[0][0] ==1 or grid[n-1][n-1]==1:
+            return -1
+
+        def bfs(src, dst):
+            q = deque()
+            q.append((src, 1))
+
+            while q:
+                (r, c), dist = q.popleft()
+                # print(r, c, dist)
+                if(r,c)==dst:
+                    return dist
+                
+                for d in dirs:
+                    next_r = r+d[0]
+                    next_c = c+d[1]
+                    if 0<=next_r<n and 0<=next_c<n and grid[next_r][next_c]==0:
+                        grid[next_r][next_c] = 1
+                        q.append(((next_r, next_c), dist+1))
+            return -1 
+
+        min_dist = bfs(src, dst)
+
+        return min_dist
