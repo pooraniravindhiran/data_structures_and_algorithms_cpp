09406f9011ed2e9d01b7112bd69f9c932cde8ad6
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-06-17 14:16:32 -0700

    Time: 4 ms (35.94%), Space: 19.3 MB (41.45%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,34 +1,43 @@
+# TC- O(mn)
+# SC- O(mn)
+
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-
-        num_fresh = 0
+        
+        m, n = len(grid), len(grid[0])
+        dirs = [-1, 0, 1, 0, -1]
+        fresh = 0
         q = deque()
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c]==1:
-                    num_fresh += 1
-                if grid[r][c]==2:
-                    q.append((r, c))
-        
-        mins = 0
-        while(q and num_fresh>0):
-            curr_size = len(q)
-            for i in range(curr_size):
-                curr_r, curr_c = q.popleft()
-                dirs = [-1, 0, 1, 0, -1]
-                for d in range(len(dirs)-1):
-                    next_r = curr_r+dirs[d]
-                    next_c = curr_c+dirs[d+1]
-                    if next_r>=0 and next_r<rows and next_c>=0 and next_c<cols and grid[next_r][next_c]==1:
-                        grid[next_r][next_c] = 2
-                        num_fresh -= 1
-                        q.append((next_r, next_c))
 
-            mins +=1
-        
-        if num_fresh>0:
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    fresh += 1
+                if grid[i][j] == 2:
+                    q.append((i, j))
+        # print(fresh)
+
+        min_time = 0
+        while q:
+            q_size = len(q)
+            anything_rotted = False
+            for i in range(q_size):
+                r, c = q.popleft()
+                grid[r][c] = 0 #mark as visited
+
+                for d in range(len(dirs)-1):
+                    next_r, next_c = r+dirs[d], c+dirs[d+1]
+                    if 0<=next_r<m and 0<=next_c<n and grid[next_r][next_c]==1:
+                        fresh -= 1
+                        grid[next_r][next_c] = 0
+                        anything_rotted = True
+                        q.append((next_r, next_c))
+            if anything_rotted:
+                min_time += 1
+
+        # print(fresh)
+        if fresh!=0:
             return -1
         
-        return mins
+        return min_time
+                
