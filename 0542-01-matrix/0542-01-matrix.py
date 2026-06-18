c3a8656713fb3258b1db3413b562a71e72603ff8
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-06-18 12:08:57 -0700

    Time: 158 ms (25.55%), Space: 22.5 MB (27.69%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,27 +1,26 @@
-# TC- O(mn)
-# SC- O(mn)
-
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        rows = len(mat)
-        cols = len(mat[0])
+        
+        m, n = len(mat), len(mat[0])
         q = deque()
+        dirs = [-1, 0, 1, 0, -1]
         visited = set()
 
-        for r in range(rows):
-            for c in range(cols):
-                if mat[r][c]==0:
-                    q.append((r,c, 0))
-                    visited.add((r,c))
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j]==0:
+                    q.append((i, j, 0))
+                    visited.add((i, j))
         
         while q:
-            curr_r, curr_c, level = q.popleft()
-            dirs = [-1, 0, 1, 0, -1]
+            r, c, level = q.popleft()
+
             for d in range(len(dirs)-1):
-                next_r = curr_r+dirs[d]
-                next_c = curr_c+dirs[d+1]
-                if 0<=next_r<rows and 0<=next_c<cols and (next_r, next_c) not in visited:
+                next_r = r+dirs[d]
+                next_c = c+dirs[d+1]
+                if 0<=next_r<m and 0<=next_c<n and (next_r, next_c) not in visited:
+                    mat[next_r][next_c] = level+1
                     visited.add((next_r, next_c))
                     q.append((next_r, next_c, level+1))
-                    mat[next_r][next_c] = level+1
         return mat
+
