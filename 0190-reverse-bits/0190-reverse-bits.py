3166f025174258f2e56d642249c9408772cd5ed1
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-06-25 11:48:08 -0700

    Time: 47 ms (51.6%), Space: 19.3 MB (9.75%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -0,0 +1,13 @@
+# TC- O(logn)
+# SC- O(1)
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        
+        res = 0
+        power = 31
+        while n:
+            res |= ((n&1)<<power)
+            n = n>>1
+            power -= 1
+        return res
