b50399f5fc6f7a82d90c8e9c0120fb14e9f47cc3
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-06-25 22:14:14 -0700

    Time: 0 ms (100%), Space: 19.4 MB (17.26%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -1,12 +1,15 @@
+# TC- O(1)
+# SC- O(1)
+
 class Solution:
     def getSum(self, a: int, b: int) -> int:
         mask = 0xffffffff
-        # in python, there's no fixed 32 bit precision. so -1 is infinite precision. so b would never become 0. so i simulate 32 bit by masking
+        # in python, there's no fixed 32 bit precision. so -1 is infinite precision. so b would never become 0 if you have a negative number in a. so i simulate 32 bit by masking
         while b:
-            temp = a ^ b
-            b = (a & b) <<1
-            b = b & mask
-            a = temp & mask
+            # print(b)
+            carry = (a & b) & mask
+            a = (a ^ b) & mask
+            b = (carry<<1) & mask
 
-        # but masking makes the negative number reallly large, so handle that
-        return a if a<=0x7fffffff else ~(a^mask)
+        # Now a is 32 bit cropped version of a signed number, but it can be positive or negative. if positive we can return as such. else we need to represent it in a different form.
+        return a if a<=0x7fffffff else a-(1<<32)
