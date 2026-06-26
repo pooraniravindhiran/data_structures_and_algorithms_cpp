d911f07ec3e574c5e83787778bd39316ca577f0e
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-06-25 22:25:50 -0700

    Time: 54 ms (10.59%), Space: 19.1 MB (92.35%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -3,16 +3,15 @@
 
 class Solution:
     def reverse(self, x: int) -> int:
-        ans = 0
         sign = -1 if x<0 else 1
         x = abs(x)
-        INT_MAX = 2**31-1
-
-        while(x):
-            digit = x %10
+        INT_MAX = (1<<31)-1
+        rev = 0
+        while x:
+            digit = x%10
             x = x//10
-            if ans > ((INT_MAX-digit)//10):
+            if rev>((INT_MAX-digit)//10):
                 return 0
-            ans = ans*10+digit
+            rev = (rev*10)+digit
 
-        return sign*ans
+        return sign*rev
