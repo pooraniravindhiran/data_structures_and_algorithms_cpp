26105ba361aebeabcc8308d64f1ddd51e906b4f1
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-06-30 13:47:48 -0700

    Time: 1964 ms (7.4%), Space: 510.9 MB (5.21%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,19 +1,18 @@
 // TC- O(nlogn)
-// SC - O(logn)
+// SC- O(logn)
 
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        // sort based on end times- we need min number of overlapping intervals - i.e max number of non overalpping intervals- this means given 2 end times, we should also retain one with smaller end time. 
-        sort(intervals.begin(), intervals.end(), [](vector<int> a, vector<int>b){return a[1]<b[1];});
+        sort(intervals.begin(), intervals.end(), [](vector<int> a, vector<int> b) {return a[1]<b[1];});
 
-        // decide if current interval is overlapping with most recent end time stored. if not, then update end time. If so, then update ans.
-        int ans = 0; int end = INT_MIN;
-        for(int i=0; i<intervals.size(); i++){
-            if(intervals[i][0] >= end)
-                end = intervals[i][1];
+        int ans = 0;
+        int end = intervals[0][1];
+        for(int i=1; i<intervals.size(); i++){
+            if (intervals[i][0]<end)
+                ans += 1;
             else
-                ans++;
+                end = intervals[i][1];
         }
         return ans;
     }
