27f40a4dcc0ea4fe387496529604c9b086a93615
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-08-07 12:38:29 -0400

    Time: 0 ms (100%), Space: 9.2 MB (41.75%) - LeetHub

---
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.cpp
@@ -0,0 +1,30 @@
+// TC- O(nm)
+// SC- O(1)
+
+class Solution {
+public:
+    int shortestWay(string source, string target) {
+        // if any letter in target is not there in src, then -1
+        // otherwise, inc letter in target, if there, keep inc till you find it src. if src ptr comes to end, then inc ans.
+
+        unordered_set<char> in_src;
+        for(char ch:source)
+            in_src.insert(ch);
+        
+        for(char ch:target){
+            if(in_src.find(ch)==in_src.end())
+                return -1;
+        }
+
+        int ans = 0, i=0;
+        while(i<target.size()){
+
+            for(char j:source){
+                if(j==target[i])
+                    i++;
+            }
+            ans++;
+        }
+        return ans;
+    }
+};
