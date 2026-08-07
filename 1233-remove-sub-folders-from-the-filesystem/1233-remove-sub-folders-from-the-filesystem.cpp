af30293c50cbe3db9a0a3af9887b67099da2123d
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-08-07 11:34:46 -0400

    Time: 40 ms (94.41%), Space: 47.2 MB (85.1%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
@@ -1,23 +1,21 @@
-// TC- O (nllogn)
-// SC- O(logn)
+// TC- O(nlogn+nL) where l is avg folder path length
+// SC- O(n)
 
 class Solution {
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
         sort(folder.begin(), folder.end());
-        // for(int i=0; i<folder.size(); i++){
-        //     cout<<folder[i]<<endl;
-        // }
-        // return vector<string> (5, "0");
 
-        vector<string> result;
-        result.push_back(folder[0]);
+        vector<string> res;
+        res.push_back(folder[0]);
+
         for(int i=1; i<folder.size(); i++){
-            string lastfolder = result.back();
-            if(folder[i].compare(0, lastfolder.size(), lastfolder)!=0 || folder[i][lastfolder.size()]!='/')
-                result.push_back(folder[i]);
-        }
+            string lastfolder = res.back();
 
-        return result;
+            // if folder doesn't start with lastfolder or it does but does'nt end with /, then add
+            if(folder[i].compare(0, lastfolder.size(), lastfolder)!=0 or folder[i][lastfolder.size()]!='/')
+                res.push_back(folder[i]);
+        }
+        return res;
     }
 };
