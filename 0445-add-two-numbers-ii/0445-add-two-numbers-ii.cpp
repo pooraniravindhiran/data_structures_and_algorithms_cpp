eb063884c281564cdd3f23b90eb138ae1946d5b6
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-08-08 22:08:18 -0400

    Time: 7 ms (15.33%), Space: 79.5 MB (20.34%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -1,5 +1,6 @@
-// TC - O(m+n)
-// SC - O(m+n)
+// TC- O(m+n)
+// SC- O(m+n)
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -12,41 +13,37 @@
  */
 class Solution {
 private:
-    stack<int> s1;
-    stack<int> s2;
+    stack<int> s1, s2;
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         while(l1){
             s1.push(l1->val);
             l1 = l1->next;
         }
-
         while(l2){
             s2.push(l2->val);
             l2 = l2->next;
         }
 
         int carry = 0;
-        int sum = 0;
-        ListNode* ans = NULL;
-        while(!s1.empty() or !s2.empty() or carry==1){
-            int a = 0;
-            int b = 0;
+        ListNode* result = nullptr;
+
+        while(!s1.empty() or !s2.empty() or carry){
+            int sum = carry;
             if(!s1.empty()){
-                a = s1.top();
+                sum += s1.top();
                 s1.pop();
             }
             if(!s2.empty()){
-                b = s2.top();
+                sum += s2.top();
                 s2.pop();
             }
-            sum = a+b+carry;
+            int digit = sum%10;
             carry = sum/10;
-            sum = sum%10;
-            ListNode* newNode = new ListNode(sum);
-            newNode->next = ans;
-            ans = newNode;
+            ListNode* curr = new ListNode(digit);
+            curr->next = result;
+            result = curr;
         }
-        return ans;
+        return result;
     }
 };
