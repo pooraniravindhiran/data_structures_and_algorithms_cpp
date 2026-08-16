0301d1582bb8ff9c8246f2f4286add974bf2acec
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-08-16 00:40:03 -0400

    Time: 147 ms (87.46%), Space: 9 MB (14.51%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,73 +1,78 @@
-// TC- O(9^e)
-// SC- O(e) where e is number of empty cells
+// TC- O(1)
+// SC- O(1)
 
 class Solution {
 private:
     vector<vector<int>> rows, cols, boxes;
-    bool solved = false;
-
-    void remove(int d, int r, int c, vector<vector<char>>& board){
-        board[r][c] = '.';
-        rows[r][d]--;
-        cols[c][d]--;
-        int b = (r/3)*3+(c/3);
-        boxes[b][d]--;
-    }
-
-    bool canplace(int d, int r, int c){
-        int b = (r/3)*3+(c/3);
-        return rows[r][d]==0 and cols[c][d]==0 and boxes[b][d]==0;
-    }
+    bool is_solved = false;
 
     void place(int d, int r, int c, vector<vector<char>>& board){
         board[r][c] = d+'0';
-        rows[r][d]++;
-        cols[c][d]++;
+        rows[r][d] = 1;
+        cols[c][d] = 1;
         int b = (r/3)*3+(c/3);
-        boxes[b][d]++;
+        boxes[b][d] = 1;
     }
 
-    void nextcell(int r, int c, vector<vector<char>>& board){
-        if(r==8 and c==8){
-            solved= true;
-            return;
-        }
+    void remove(int d, int r, int c, vector<vector<char>>& board){
+        board[r][c] = '.';
+        rows[r][d] = 0;
+        cols[c][d] = 0;
+        int b = (r/3)*3+(c/3);
+        boxes[b][d] = 0;
+    }
 
-        else if(c==8)
-            backtrack(r+1, 0, board);
-        else
-            backtrack(r, c+1, board);
+    bool can_place(int d, int r, int c){
+        int b= (r/3)*3+(c/3);
+        return rows[r][d]==0 and cols[c][d]==0 and boxes[b][d]==0;
     }
 
     void backtrack(int r, int c, vector<vector<char>>& board){
-        if(board[r][c]!='.'){
-            nextcell(r, c, board);
+        // check if end then solved return
+        if(r==9){
+            is_solved = true;
             return;
         }
+
+        // find next cell and keep
+        int nr = r;
+        int nc = c+1;
+        if(nc==9){
+            nr = nr+1;
+            nc = 0;
+        }
+
+        // if number, goto next cell
+        if(board[r][c]!='.')
+            backtrack(nr, nc, board);
+
+        // if empty, place backtrack
         else{
-            for(int d=1; d<=9; d++){
-                if(!canplace(d, r, c))
+            for(int i=1; i<10; i++){
+                if(!can_place(i, r, c))
                     continue;
-                place(d, r, c, board);
-                nextcell(r, c, board);
-                if(!solved)
-                    remove(d, r, c, board);
+                
+                // if valid, place it, backtrack and if not valid, remove
+                place(i, r, c, board);
+                backtrack(nr, nc, board);
+                if(is_solved)
+                    return;
+                remove(i, r, c, board);
             }
         }
     }
 
 public:
     void solveSudoku(vector<vector<char>>& board) {
-        rows.assign(9, vector<int>(10, 0));
-        cols.assign(9, vector<int>(10, 0));
-        boxes.assign(9, vector<int>(10, 0));
+        rows.assign(9, vector<int> (10, 0));
+        cols.assign(9, vector<int> (10, 0));
+        boxes.assign(9, vector<int> (10, 0));
 
-        for(int r=0; r<9; r++){
-            for(int c=0; c<9; c++){
-                if(board[r][c]!='.'){
-                    int d = board[r][c]-'0';
-                    place(d, r, c, board);
-                }  
+        // first update rows, cols, boxes with existing numbers
+        for(int i=0; i<board.size(); i++){
+            for(int j=0; j<board[0].size(); j++){
+                if(board[i][j]!='.')
+                    place(board[i][j]-'0', i, j, board);
             }
         }
 
