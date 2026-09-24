e704e01c1822868743f21a5e27571f5a889c2bb4
Poorani Ravindhiran <63573162+pooraniravindhiran@users.noreply.github.com>
2026-09-23 23:43:09 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.cpp
+++ 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.cpp
@@ -1,28 +1,29 @@
-// TC- O(slogs)
-// SC- O(s)
+// SC- O(nlogn)
+// TC- O(n)
 
 class Solution {
 public:
     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
-        stations.push_back({target, 0});
-        int currfuel = startFuel;
         int currpos = 0;
+        int currfuel = startFuel;
+        stations.push_back({target, 0});
         priority_queue<int> max_heap;
-        int min_stops = 0;
+
+        int ans = 0;
 
         for(int i=0; i<stations.size(); i++){
-            int loc = stations[i][0], fuel = stations[i][1];
-            currfuel = currfuel - (loc-currpos);
-            while(currfuel<0){
+            currfuel = currfuel - (stations[i][0]-currpos);
+            currpos = stations[i][0];
+
+            if(currfuel<0){
                 if(max_heap.empty())
                     return -1;
                 currfuel += max_heap.top();
                 max_heap.pop();
-                min_stops ++;
+                ans++;
             }
-            max_heap.push(fuel);
-            currpos = loc;
+            max_heap.push(stations[i][1]);
         }
-        return min_stops;
+        return ans;
     }
 };
